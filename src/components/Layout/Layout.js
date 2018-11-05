e7476028dacae05c405581a60067e7b879668ec0
Pushkar Shembekar <pushembekar@gmail.com>
2018-11-04 23:34:13 -0500

    Added the css module file and incorporated some additonal items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Auxilary from '../../hoc/Auxilary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Auxilary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={ classes.Content }>
             {props.children}
         </main>
     </Auxilary>
