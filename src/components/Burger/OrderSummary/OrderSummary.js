e65a2f45c01850f851c64d5c04805545bbcd550c
Pushkar Shembekar <pushembekar@gmail.com>
2018-11-08 22:16:10 -0500

    Added the modal checkout component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Auxilary from '../../../hoc/Auxilary';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+        })
+    return (
+        <Auxilary>
+            <h3>Your Order</h3>
+            <p>Delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Auxilary>
+    )
+}
+
+export default orderSummary;
