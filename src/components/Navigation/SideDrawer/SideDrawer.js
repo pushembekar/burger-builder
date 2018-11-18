e165cede6e0524d3e9086e0a5da6a33f6412fd25
Pushkar Shembekar <pushembekar@gmail.com>
2018-11-17 21:34:30 -0500

    Added various functionalities and the hamburger icon

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationsItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxilary from '../../../hoc/Auxilary';
+
+import classes from './SideDrawer.module.css';
+
+const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <Auxilary>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxilary>
+    );
+};
+
+export default sideDrawer;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationsItems'
+import NavigationItems from '../NavigationItems/NavigationsItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems></NavigationItems>
         </nav>
     </header>
